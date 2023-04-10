9167ad47e221e7fca0ed9802e6cc52c0d4f1619d
Varga Gábor <gabor.varga@frontendart.com>
2023-04-10 17:34:10 +0200

    refact FloatingValue.cs

--- GUI/GUI/FloatingValue.cs
+++ GUI/GUI/FloatingValue.cs
@@ -1,34 +1,31 @@
 using Godot;
-using System;
 
 namespace VampireSurvivorsLike {
 
     public class FloatingValue : Position2D {
 
-        private Tween tween { get; set; }
-        private Label DamageValue { get; set; }
-        private int amount { get; set; }
-        private Color color { get; set; }
+        private int value;
+        private Color color;
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready() {
-            this.tween = GetNode<Tween>("Tween");
-            this.DamageValue = GetNode<Label>("Label");
+            Tween tween = GetNode<Tween>("Tween");
+            Label valueLabel = GetNode<Label>("Label");
 
-            this.DamageValue.Text = amount.ToString();
-            this.DamageValue.Modulate = this.color;
+            valueLabel.Text = this.value.ToString();
+            valueLabel.Modulate = this.color;
             Vector2 newPos = this.GlobalPosition;
-            newPos.y -= 30;
-            this.tween.InterpolateProperty(this, "position", this.GlobalPosition, newPos, 0.5f);
-            this.tween.InterpolateProperty(this.DamageValue, "modulate:a", 1, 0, 1);
-            this.tween.Start();
+            newPos.y -= 40;
+            tween.InterpolateProperty(this, "position", this.GlobalPosition, newPos, 0.4f);
+            tween.InterpolateProperty(valueLabel, "modulate:a", 1, 0, 1);
+            tween.Start();
         }
 
-        public static void CreateFloatingValue(Vector2 position, Color color, int amount, Node parent) {
+        public static void CreateFloatingValue(Vector2 position, Color color, int value, Node parent) {
             FloatingValue floatingValue = ResourceLoader.Load<PackedScene>("res://GUI/GUI/FloatingValue.tscn")
                 .Instance<FloatingValue>();
             floatingValue.color = color;
-            floatingValue.amount = amount;
+            floatingValue.value = value;
             position.y -= 20;
             floatingValue.GlobalPosition = position;
             parent.CallDeferred("add_child", floatingValue);
@@ -36,7 +33,7 @@ namespace VampireSurvivorsLike {
 
 
         public void TweenCompleted() {
-            CallDeferred("queue_free");
+            this.CallDeferred("queue_free");
         }
 
     }
