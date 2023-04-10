bca6ffb1efb6b21c44953ce3c931a65f79294c46
Varga Gábor <gabor.varga@frontendart.com>
2023-04-10 18:12:53 +0200

    refact LevelUpScreen.cs PauseScreen.cs RewardItemContainer.cs

--- GUI/GUI/PauseScreen.cs
+++ GUI/GUI/PauseScreen.cs
@@ -4,13 +4,6 @@ namespace VampireSurvivorsLike {
 
     public class PauseScreen : CenterContainer {
 
-        private LevelUpScreen levelUpScreen;
-
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready() {
-            this.levelUpScreen = GetNode<LevelUpScreen>("../LevelUpScreen");
-        }
-
         public void TogglePauseGame(bool isPaused) {
             this.Visible = isPaused;
             if (isPaused) {
@@ -21,15 +14,15 @@ namespace VampireSurvivorsLike {
         public void OnResumeButtonPressed() {
             if (GameStateManagerSingleton.Instance.IsMultiplayer) {
                 this.GetTree().Root.GetNode<Main>("Main").Rpc(nameof(Main.TogglePauseGame), false);
-                return;
+            } else {
+                this.GetTree().Root.GetNode<Main>("Main").TogglePauseGame(false);
             }
-            this.GetTree().Root.GetNode<Main>("Main").TogglePauseGame(false);
         }
 
         public void OnSettingsButtonPressed() {
             this.Visible = false;
         }
-        
+
         public void OnQuitButtonPressed() {
             GetTree().Paused = false;
             this.GetTree().Root.GetNode<Network>("Network").ConnectionClosed();
--- GUI/GUI/RewardItemContainer.cs
+++ GUI/GUI/RewardItemContainer.cs
@@ -1,32 +1,22 @@
 using Godot;
-using System;
 
 namespace VampireSurvivorsLike {
 
     public class RewardItemContainer : Control {
 
-        public int Index {get; private set;}
-        private Button button;
-        private Label label;
+        private int index;
 
         [Signal] public delegate void ButtonPressed(int index);
 
-        public override void _Ready() {
-            this.Index = -1;
-            this.button = GetChild<Button>(0);
-            this.label = GetChild<Label>(1);
-        }
-
         public void SetItemContainer(int index, Texture icon, string text, LevelUpScreen receiver) {
-            this.Index = index;
-            this.button.Icon = icon;
-            this.label.Text = text;
-            Connect(nameof(ButtonPressed), receiver, nameof(receiver.OnRewardSelected));
+            this.index = index;
+            this.GetChild<Button>(0).Icon = icon;
+            this.GetChild<Label>(1).Text = text;
+            this.Connect(nameof(ButtonPressed), receiver, nameof(receiver.OnRewardSelected));
         }
-
 
         public void OnButtonPressed() {
-            EmitSignal(nameof(ButtonPressed), this.Index);
+            this.EmitSignal(nameof(ButtonPressed), this.index);
         }
 
     }
