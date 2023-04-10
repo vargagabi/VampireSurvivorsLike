e5bad0349c3f3b56e006089697865a669a04382a
Varga Gábor <gabor.varga@frontendart.com>
2023-04-10 17:53:00 +0200

    refact HUD.cs

--- GUI/GUI/HUD.cs
+++ GUI/GUI/HUD.cs
@@ -5,53 +5,39 @@ namespace VampireSurvivorsLike {
 
     public class HUD : Control {
 
-        private Label timeLabel;
-
-        // private Label HpNumber { get; set; }
-        // private TextureProgress expBar;
-        // private Label LevelNumber { get; set; }
-        internal float ElapsedTime { get; set; }
-        private HBoxContainer ItemControl { get; set; }
-        private int nextEmpyItemFrame = 0;
+        private float elapsedTime;
+        private HBoxContainer itemControl;
+        private int nextEmptyItemFrame = 0;
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready() {
             GD.Print("HUD Ready...");
-
-            // ItemManagerSingleton.Instance.Hud = this;
-            this.ElapsedTime = 0;
-            this.timeLabel = GetNode<Label>("TimerControl/TimeLabel");
-
-            // this.HpNumber = GetNode<Label>("HpControl/HpNumber");
-            // this.LevelNumber = GetNode<Label>("LevelControl/LevelNumber");
-            // this.expBar = GetNode<TextureProgress>("ExpBar");
-            this.ItemControl = GetNode<HBoxContainer>("ItemControl");
-
-            // this.expBar.Value = 0;
-            // this.LevelNumber.Text = "0";
+            this.elapsedTime = 0;
+            this.itemControl = GetNode<HBoxContainer>("ItemControl");
             GetNode<Label>("GoldContainer/Label").Text = "0";
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(float delta) {
-            this.ElapsedTime += delta;
-            this.timeLabel.Text = TimeSpan.FromSeconds(this.ElapsedTime).ToString("hh':'mm':'ss");
+            this.elapsedTime += delta;
+            GetNode<Label>("TimerControl/TimeLabel").Text =
+                TimeSpan.FromSeconds(this.elapsedTime).ToString("hh':'mm':'ss");
         }
 
         public void SetItem(Texture icon, int level) {
-            //Not new  item
-            foreach (Control frame in this.ItemControl.GetChildren()) {
-                if (frame.GetChild<TextureRect>(1).Texture != null &&
-                    frame.GetChild<TextureRect>(1).Texture.Equals(icon)) {
-                    frame.GetChild<Label>(2).Text = level.ToString();
-                    return;
+            //Already have this item
+            foreach (Control frame in this.itemControl.GetChildren()) {
+                if (frame.GetChild<TextureRect>(1).Texture == null ||
+                    !frame.GetChild<TextureRect>(1).Texture.Equals(icon)) {
+                    continue;
                 }
+                frame.GetChild<Label>(2).Text = level.ToString();
+                return;
             }
 
             //New item
-            Control itemFrame = this.ItemControl.GetChild<Control>(this.nextEmpyItemFrame++);
-            TextureRect iconTexture = itemFrame.GetChild<TextureRect>(1);
-            iconTexture.Texture = icon;
+            Control itemFrame = this.itemControl.GetChild<Control>(this.nextEmptyItemFrame++);
+            itemFrame.GetChild<TextureRect>(1).Texture = icon;
             itemFrame.GetChild<Label>(2).Text = level.ToString();
         }
 
@@ -67,12 +53,8 @@ namespace VampireSurvivorsLike {
             this.GetNode<Label>("LevelControl/LevelNumber").Text = value.ToString();
         }
 
-        public void OnRewardSelected(int index) {
-            // this.IncreaseLevel(null);
-        }
-
         public void SetGold(int value) {
-            GetNode<Label>("GoldContainer/Label").Text = value.ToString();
+            this.GetNode<Label>("GoldContainer/Label").Text = value.ToString();
         }
 
     }
